2e6fe730cb6fc109a74d6a8c4ca84339e53fbd4d
hiratara <hiratara@cpan.org>
2009-12-25 12:10:30 +0900

    Moved codes to the .pm file.

--- lib/RuntUp.pm
+++ lib/RuntUp.pm
@@ -1,7 +1,53 @@
 package RuntUp;
-use strict;
-use warnings;
+use Any::Moose;
+use File::Spec;
 our $VERSION = '0.01';
+
+
+has server => (
+	is  => 'ro',
+	isa => 'Str',
+	required => 1,
+);
+
+has paths => (
+	is  => 'ro',
+	isa => 'ArrayRef[Str]',
+	required => 1,
+);
+
+
+around BUILDARGS => sub {
+	my $meth  = shift;
+	my $class = shift;
+
+	my $server = shift @_;
+
+	return $class->$meth(
+		server  => $server,
+		paths   => [ @_ ],
+	);
+};
+
+
+__PACKAGE__->meta->make_immutable;
+no  Any::Moose;
+
+
+sub run {
+	my $self = shift;
+	foreach ( @{ $self->paths } ) {
+		my $abs = File::Spec->rel2abs($_);
+		my ($path) = ( $abs =~ m{/(?:SV[KN]|GIT)_work/some_project/[^/]+(/path/to/.+)$} ) 
+			or next;
+		system(
+			'scp', '-p',
+			$abs,
+			'user@host:' . $path,
+		);
+	}
+}
+
 
 1;
 __END__
