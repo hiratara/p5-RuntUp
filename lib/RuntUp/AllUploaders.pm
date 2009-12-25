4580a1b562e4e612cb048402052dd465bcc00c0f
hiratara <hiratara@cpan.org>
2009-12-25 14:36:07 +0900

    Added FTP uploader.

--- lib/RuntUp/AllUploaders.pm
+++ lib/RuntUp/AllUploaders.pm
@@ -26,5 +26,39 @@ sub upload{
 }
 
 
+package RuntUp::Uploader::FTP;
+use Any::Moose;
+use Net::FTP;
+
+has [qw/user host pass/] => ( is => 'ro', isa => 'Str', required => 1 );
+has ftp => (is => 'rw', isa => 'Net::FTP');
+
+__PACKAGE__->meta->make_immutable;
+no  Any::Moose;
+
+sub upload{
+	my $self = shift;
+	my ( $local, $remote ) = @_;
+
+	! -f $local and do { warn "Support only file: ", $local; return; };
+	my $executable = -x $local;
+
+	unless( $self->ftp ){
+		my $ftp = Net::FTP->new( $self->host ) or die;
+		$ftp->login( $self->user, $self->pass ) or die $ftp->message;
+		$ftp->binary;
+		$self->ftp( $ftp );
+	}
+
+	# TODO: The server may not be same OS with the client.
+	my ( $vol, $dir, $file ) = File::Spec->splitpath( $remote );
+	$self->ftp->mkdir( $dir, 1 );
+	$self->ftp->cwd( $dir );
+	my $result = $self->ftp->put( $local );
+	$self->ftp->site( qw(CHMOD 755), $result ) if $executable;
+
+	print "put: ", $result, "\n";
+}
+
 
 1;
